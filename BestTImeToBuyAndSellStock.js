a8e8596864b08ffd986f34b36745d416f95c1abd
Rafael Silva <devsilva.rafael@gmail.com>
2022-12-06 18:26:08 -0300

    feat: #121(easy) Best Time to Buy and Sell Stock algorithm solved.

--- BestTImeToBuyAndSellStock.js
+++ BestTImeToBuyAndSellStock.js
@@ -1,23 +1,20 @@
-var maxProfit = function (prices) {
+var maxProfit = function(prices) {
     let buyDay = prices[0];
     let profit = 0;
-
-    for (let i = 1; i < prices.length; i++) {
-        if (buyDay > prices[i]) {
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] < buyDay) {
             buyDay = prices[i];
         }
 
-        const currentProfit = prices[i] - buyDay;
+        let currentProfit = prices[i] - buyDay;
 
-        if (currentProfit > profit) {
+        if(currentProfit > profit) {
             profit = currentProfit;
         }
     }
-
     return profit;
-};
+}
 
+const test = maxProfit([7,1,5,3,6,4]);
 
-const test = maxProfit([1,2]);
-
-console.log(test)
+console.log(test);
