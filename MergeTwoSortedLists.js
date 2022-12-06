55e4c976074d416a39add18f8898b382ef5f4442
Rafael Silva <devsilva.rafael@gmail.com>
2022-12-06 08:21:56 -0300

    chore: add comments.

--- MergeTwoSortedLists.js
+++ MergeTwoSortedLists.js
@@ -3,26 +3,18 @@ function ListNode(val, next) {
     this.next = (next === undefined ? null : next)
 }
 
-var mergeTwoLists = function(list1, list2) {
-    // Criando nó "cabeça".
+var mergeTwoLists = function (list1, list2) {
     let head = new ListNode(null);
-    let prev = head;
 
     while(list1 && list2) {
-        if (list1.val < list2.val) {
-            prev.next = list1;
+        if(list1.val < list2.val) {
+            head.next = list1;
             list1 = list1.next;
         } else {
-            prev.next = list2;
+            head.next = list2;
             list2 = list2.next;
         }
-        prev = prev.next;
     }
-
-    if(!list1) { prev.next = list2 }
-    if(!list2) { prev.next = list1 }
-
-    return head.next;
 }
 
-const mergedList = mergeTwoLists([1,2,3], [2,3,4]);
+mergeTwoLists([1,3,5], [2,3,5])
